985cde0c42782c57284d0a686a1a50a3f30f3b22
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-09-19 21:39:40 +0100

    initial commit

---
+++ signin_form.py
@@ -0,0 +1,14 @@
+
+
+#learning variables
+name = "confidence chinecherem"
+age = 20
+email = "sconfidencechinecherem@gmail.com"
+print(name)
+print(age)
+print(email)
+#adding string to the variables
+print("My name is " , name)
+print("I am " , age , "years old")
+print("Here is my email address", email)
+
