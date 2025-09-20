d34a883a44c8aae307af2c21d58dd45d03feb3e8
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-09-20 20:14:00 +0100

    collection of user details

--- beginner-python-projects/signin_form.py
+++ beginner-python-projects/signin_form.py
@@ -12,3 +12,8 @@ print("My name is " , name)
 print("I am " , age , "years old")
 print("Here is my email address", email)
 
+#collecting of user details
+full_name = input("enter your full name: ")
+real_age = input("enter your age:")
+email_address = input('enter your email address: ')
+print(full_name,real_age,email_address)
