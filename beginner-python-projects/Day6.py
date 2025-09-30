5ba53105bd8b1a21ecf2ed5c26ceca52b5936f5b
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-09-30 23:10:08 +0100

    folder modification

--- beginner-python-projects/Day6.py
+++ beginner-python-projects/Day6.py
@@ -1,71 +1,58 @@
-#Multi user system and unique user system
+#Limited login attempt added muilti user 
 
 #variables
 users = {}
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
 
-
-#conditional statement for sign up
-if age < 18:
-    print("Your are not of age", name)
-else:
-    email = input("Enter your email: ")
-    if email in users:
-        print("Email already exists! Try logging in instead.")
-        # --- Login Section ---
-        attempts = 3
-        while attempts > 0:
-            login_email = input("Enter your email: ")
-            login_password = input("Enter your password: ")
-
-            if login_email in users and login_password == users[login_email]["password"]:
-                print("Welcome back,", users[login_email]["name"], "Login successful ✅")
+while True:
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+    if age < 18:
+        print("Not of age!")
+    else:
+        email = input("Enter your email: ")
+        if email in users:
+            print("Email already exist!, Try loggin instead")
+            continue
+        else:
+            password = input("Create a password: ")
+            if len(password) < 6:
+                print("Pasword is too short!, Atleast 6 character long")
+                continue
+            confirm_password = input("Confirm your password: ")
+            if password != confirm_password:
+                print("Password did not match!, Try again")
+            else:
+                users[email] = {"name": name, "age": age, "password": password }
+                print("Registration successful", name)
+                #login section
+                print("\n Login Section")
+                attempt = 3
+                login_email = input("Enter your registered email: ")
+                while attempt > 0:
+                    if login_email not in users:
+                        attempt -= 1
+                        if attempt > 0:
+                            print(f"Email not found, You have", attempt, " left. Try again")
+                            login_email = input("Enter your email: ")
+                        else:
+                            print("Too many failed attempts!, Please Try creating a new account")
+                        continue
+                    password_attempt = 3
+                    while password_attempt > 0:
+                        login_password = input("Enter your password: ")
+                        if login_password != users[login_email]["password"]:
+                            password_attempt -= 1
+                            if password_attempt > 0:
+                                print("Wrong password, Please re-enter the correct password and you have", password_attempt, "remaining!")
+                            else:
+                                print("Too many failed attempts, your account is locked")
+                        else:
+                            print("Login successful", users[login_email]["name"])
+                            password_attempt = 0
+                            attempt = 0
                 break
-            else:
-                attempts -= 1
-                if attempts > 0:
-                    print("Invalid details. You have", attempts, "attempts left.")
-                else:
-                    print("Too many failed attempts. Account locked ❌") 
-    else:
-        password = input("Create a password ")
-        confirm_password = input("Confirm your password ")
-        if len(password) < 6:
-            print("Password is too short!, It must be atleast 6 character long")
-        elif password != confirm_password:
-            print("Password does not match!")
-        else:
-            users[email] = {"name": name, "password": password}
-            print("Registration successful", name)
+                    
+                   
 
-#conditional statements for login
-    print("\n Continue to Login ")
-    login_email = input("Enter your email: ")
-    if login_email not in  users:
-        print("Incorrect email")
-    else:
-        login_password = input("Enter your password: ")
-        if len(login_password) < 6:
-         print("Password is too short!, It must be atleast 6 character long!")
-        elif login_password != users[login_email]["password"]:
-         print("Incorrect password")
-        else:
-         print("Welcome back!", users[login_email]["name"], "Login successful")
-    attempts = 2
-    while attempts > 0:
-        print("Try logging in again")
-        login_email = input("Enter your email ")
-        if login_email not in users:
-            print("Incorrect email")
-        else:
-            login_password = input("Enter your password ")
-            if len(login_password) < 6:
-                print("Password is too short!, It must be atleast 6 character long!")
-            elif login_password != users[login_email]["password"]:
-                print("Incorrect password")
-            attempts -= 1
-            if attempts > 0:
-                print("Incorrect details ", name, "you have", attempts, "attempts left")
-            else:
-                print('Too many failed attempts, your account is locked')
+    
+          
